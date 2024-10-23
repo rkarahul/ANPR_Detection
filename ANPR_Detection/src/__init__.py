68ef8cdfa671f350f1da22ddfdce0532b24ee346
Rahul Kumar <90387491+rkarahul@users.noreply.github.com>
2024-10-23 15:51:35 +0530

    Delete ANPR_Detection directory
    delete

--- ANPR_Detection/src/__init__.py
+++
@@ -1,8 +0,0 @@
-# This file marks the src directory as a Python package.
-
-# You can also import your main class or functions here if you want to make them
-# accessible when the package is imported.
-
-from .anpr_system import ANPRSystem
-
-__all__ = ["ANPRSystem"]
